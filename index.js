19f3296804d1899926a52d03ff311ace685d68f3
Maksym <maxim4110@gmail.com>
2021-03-10 19:10:24 +0200

    fix validation

--- index.js
+++ index.js
@@ -7,15 +7,43 @@ function getLogoSrc(value) {
 	return 'mastercard_logo.svg'
 }
 
-function isCardNumberValid(inputValue) {
+function ccFormat(value) {
+	const v = value.replace(/\s+/g, '').replace(/[^0-9]/gi, '')
+	const matches = v.match(/\d{4,16}/g)
+	const match = matches && matches[0] || ''
+	const parts = []
+
+	for (let i = 0, len = match.length; i < len; i += 4) {
+		parts.push(match.substring(i, i + 4))
+	}
+
+	return parts.length ? parts.join(' ') : value
+}
+
+const luhnCheck = (function (arr) {
+	return function (ccNum) {
+		let
+			len = ccNum.length,
+			bit = 1,
+			sum = 0,
+			val;
+
+		while (len) {
+			val = parseInt(ccNum.charAt(--len), 10);
+			sum += (bit ^= 1) ? arr[val] : val;
+		}
+
+		return sum && sum % 10 === 0;
+	};
+}([0, 2, 4, 6, 8, 1, 3, 5, 7, 9]));
+
+function isCardNumberValid(value) {
+	const inputValue = value.replaceAll(/\s/g,'')
+
 	if (inputValue.length !== 16)
 		return false
-	if (inputValue.charAt(0) !== '4' && inputValue.charAt(0) !== '5')
-		return false
 
-	const sum = inputValue.split('').map(ch => parseInt(ch, 10)).reduce((a, b) => a + b, 0)
-
-	return sum % 10 === 0
+	return luhnCheck(inputValue)
 }
 
 function validateCardNumber(value) {
@@ -30,4 +58,5 @@ cardNumber.oninput = function (e) {
 	const val = e.target.value
 	document.getElementById('card_logo_id').src = getLogoSrc(val)
 	validateCardNumber(val)
+	cardNumber.value = ccFormat(val)
 }
